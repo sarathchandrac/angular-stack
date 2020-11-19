e5a5f2af2360fbc0192ed7b5ce835c9b05aa5876
Karra, Umesh (uk179v) <uk179v@att.com>
2020-11-19 20:27:00 +0530

    changes added in todo list with description for each todo

--- apps/dashboard/src/app/todolist/todolist.component.ts
+++ apps/dashboard/src/app/todolist/todolist.component.ts
@@ -8,15 +8,21 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class TodolistComponent implements OnInit{
     
   constructor() {}
-  todolist = [{"todotitle":"Welcome todolist"}]
+  todolist = [{"todotitle":"Welcome todolist", desc:"", showdesc:false}]
   todotitle:string
+  desc:string
   ngOnInit(): void {
     
   }
   addnewtodolist(title){
-    var temp = {'todotitle':title}
+    var temp = {'todotitle':title, desc:"", showdesc:false}
     this.todolist.push(temp);
     this.todotitle = "";
   }
- 
+  changestatus(key){
+    this.todolist[key].showdesc = true;
+  }
+  adddescriptionstodo(key){
+    this.todolist[key].desc = this.desc
+  }
 }
